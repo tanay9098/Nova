daff17f8fa6d4524b7cb0a48df1334eedf2436e2
tanay9098 <tanaydwivedi678@gmail.com>
2026-07-15 15:38:43 +0530

    feat:implemented rudimentory message history

--- chatmodels/test.py
+++ chatmodels/test.py
@@ -9,13 +9,17 @@ from langchain_mistralai import ChatMistralAI
 
 model=ChatMistralAI(model_name="mistral-small-2506", temperature=0.9, max_tokens=150)
 
+messages_history=[]
+
 print("Model initialized:", model)
 
 while True:
     print("Type 'exit' to quit.")
 
     prompt=input("You: ")
+    messages_history.append(prompt)
     if prompt.lower() == "exit":
         break
-    response = model.invoke(prompt)
+    response = model.invoke(messages_history)
+    messages_history.append(response.content)
     print("Response:", response.content)
